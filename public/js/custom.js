93c3f8c80b1c8eedab028b6ddadc84863b6d5c4d
Handy Wardhana <handy@joineka.com>
2020-05-22 00:17:58 +0700

    update fontawesome

--- public/js/custom.js
+++ public/js/custom.js
@@ -118,25 +118,4 @@ $('.counter').counterUp({
     });
 
 
-/*----- Subscription Form ----- */
 
-$(document).ready(function() {
-     // jQuery Validation
-     $("#chimp-form").validate({
-         // if valid, post data via AJAX
-         submitHandler: function(form) {
-             $.post("https://send.pageclip.co/LYKFuPPLfvccGkWzBQeHDIOuVOxgIEhN/contacts", { email: $("#chimp-email").val() }, function(data) {
-                 $('#response').html(data);
-             });
-         },
-         // all fields are required
-         rules: {
-             email: {
-                 required: true,
-                 email: true
-             }
-         }
-     });
- });
-
-});
